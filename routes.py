b459441116fbb9449cb69a657e972bf2984254d0
alexahildebrand <93009739+alexahildebrand@users.noreply.github.com>
2021-11-29 15:49:11 -0800

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,9 +1,9 @@
 from crafter import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from crafter.models import Course, User, Professor, Section
-from crafter.forms import RegisterForm, LoginForm
+from crafter.forms import RegisterForm, LoginForm, EnrollClassForm, DropClassForm
 from crafter import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route('/')
@@ -11,11 +11,39 @@ from flask_login import login_user, logout_user, login_required
 def home_page():
     return render_template('home.html')
 
-@app.route('/courses')
+@app.route('/courses',methods=['GET', 'POST'])
 @login_required
 def schedule_page():
-    courses = Course.query.all()
-    return render_template('courses.html', courses=courses)
+    enroll_form = EnrollClassForm()
+    drop_form = DropClassForm()
+    if request.method == "POST":
+        # Enroll Course Logic
+        enrolled_course = request.form.get('enrolled_course')
+        e_course_object = Course.query.filter_by(name=enrolled_course).first()
+        if e_course_object:
+            if current_user.can_enroll(e_course_object):
+                e_course_object.enroll(current_user)
+                flash(f'Congratulations! You enrolled in {e_course_object}', category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough units to enroll in {e_course_object.name}", category='danger')
+
+        # Drop Course Logic
+        dropped_course = request.form.get('dropped_course')
+        d_course_object = Course.query.filter_by(name=dropped_course).first()
+        if d_course_object:
+            if current_user.can_drop(d_course_object):
+                d_course_object.drop(current_user)
+                flash(f'Success! You dropped {d_course_object}!', category='success')
+            else:
+                flash(f"Something went wrong with dropping {d_course_object.name}", category='danger')
+
+        return redirect(url_for('schedule_page'))
+
+    if request.method == "GET":
+        courses = Course.query.filter_by(student=None)
+        enrolled_courses = Course.query.filter_by(student=current_user.id)
+        return render_template('courses.html', courses=courses, enroll_form=enroll_form, enrolled_courses=enrolled_courses,
+                               drop_form=drop_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -27,7 +55,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         login_user(user_to_create)
-        flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
+        flash(f'Account created successfully! Hey, {user_to_create.username}!', category='success')
         return redirect(url_for('schedule_page'))
     if form.errors != {}:  # if there are not errors from the validation
         for err_msg in form.errors.values():
@@ -111,7 +139,7 @@ def login_page():
                 attempted_password=form.password.data
         ):
             login_user(attempted_user)
-            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            flash(f'Success! Hey, {attempted_user.username}!', category='success')
             return redirect(url_for('schedule_page'))
         else:
             flash('Username and password are not match! Please try again', category='danger')
